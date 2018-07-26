2a6fcae6f05004b18f12074f1fffd4a751bfa8c1
Benjamin Wiese <benjamin.wiese@methodpark.de>
2018-07-26 12:35:59 +0200

    Add desktop-save-mode, cua-mode, magit

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+;; run emacs as client server
+(require 'server)
+(unless (server-running-p)
+(server-start))
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -8,5 +13,21 @@
 (unless (package-installed-p 'use-package)
 	(package-refresh-contents)
 	(package-install 'use-package))
+(setq use-package-always-ensure t)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+(desktop-save-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (spacemacs-theme magit which-key use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
