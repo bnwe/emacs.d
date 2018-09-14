54bbd2db7dc8da566a86f92bc69e9b1d1ec4e226
Benjamin Wiese <benjamin.wiese@methodpark.de>
2018-09-14 13:29:20 +0200

    Add avy, ace window and key-chord

--- init.el
+++ init.el
@@ -35,10 +35,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
  '(package-selected-packages
    (quote
-    (projectile spacemacs-theme magit which-key use-package)))
- '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t)))))
+    (key-chord projectile spacemacs-theme magit which-key use-package))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
