ac380fdd5800eeb85c0ca9875d5bcb67919700ec
Benjamin Wiese <benjamin.wiese@methodpark.de>
2018-09-14 10:03:39 +0200

    Configure autosave to use separate directory

--- init.el
+++ init.el
@@ -37,7 +37,9 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (projectile spacemacs-theme magit which-key use-package))))
+    (projectile spacemacs-theme magit which-key use-package)))
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t)))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
