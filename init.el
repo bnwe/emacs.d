e9d51e3262f1c17254277fdb8c889f8236dbfa4b
Benjamin Wiese <benjamin.wiese@methodpark.de>
2020-06-23 21:06:46 +0200

    Remove spacemacs and magit

--- init.el
+++ init.el
@@ -35,9 +35,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
- '(package-selected-packages
-   '(key-chord projectile spacemacs-theme magit which-key use-package)))
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
+ '(package-selected-packages (quote (key-chord projectile which-key use-package))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
